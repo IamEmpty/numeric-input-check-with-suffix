3dd1b31e075a37397fdbaaca1ead993472ae2086
IamEmpty <dustonroad@gmail.com>
2015-01-28 02:39:24 +0300

    init

---
+++ main.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+
+	$('.numbersOnly').keyup(function () {
+
+		var suffix;
+		var $this = $(this);
+
+		if( $this.hasClass('weight') ) {
+			suffix = " kg";
+		}
+
+		if( $this.hasClass('volume') ) {
+			suffix = " m³";
+		}
+
+	    if (this.value != this.value.replace(/[^0-9\.]/g, '')) {
+			this.value = this.value.replace(/[^0-9\.]/g, '') + suffix;
+	    }
+	});
+});
